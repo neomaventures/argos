c5e24a81db14219788193df45ae54ec6fa18de27
dannybster <dannybster@gmail.com>
2026-04-30 08:46:22 +0100

    Initial commit

---
+++ libs/package-template/src/example.module.spec.ts
@@ -0,0 +1,14 @@
+import { Test } from "@nestjs/testing"
+
+import { ExampleModule } from "@lib"
+
+describe("ExampleModule", () => {
+  it("should compile the module", async () => {
+    const module = await Test.createTestingModule({
+      imports: [ExampleModule],
+    }).compile()
+
+    expect(module).toBeDefined()
+    expect(module).toBeInstanceOf(Object)
+  })
+})
